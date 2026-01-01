bc7aa3d2d72b5719e908e8140915f87bf8e63312
chenh <chenhaitry64@gmail.com>
2026-01-01 09:53:24 +0700

    Update dependencies and clean up Vuetify type declarations

--- src/types/vuetify.d.ts
+++ src/types/vuetify.d.ts
@@ -1,16 +1,6 @@
-// types/vuetify-hooks.d.ts
 import type { createVuetify } from 'vuetify'
-import type { HookResult } from '@nuxt/schema'
-import type { ModuleOptions } from './options'
-import type { VuetifyHookContext } from './hooks'
 
 declare module '#app' {
-  interface RuntimeNuxtHooks {
-    'vuetify:register': (registerModule: (options: Partial<ModuleOptions>) => void) => HookResult
-    'vuetify:before-create': (context: VuetifyHookContext) => HookResult
-    'vuetify:configuration': (context: VuetifyHookContext) => HookResult
-    'vuetify:ready': (vuetify: ReturnType<typeof createVuetify>) => HookResult
-  }
   interface PublicRuntimeConfig {
     vuetify: VuetifyRuntimeConfig
   }
@@ -21,21 +11,11 @@ declare module '#app' {
 }
 
 declare module '@nuxt/schema' {
-  interface NuxtHooks {
-    'vuetify:before-create': (context: VuetifyHookContext) => HookResult
-    'vuetify:configuration': (context: VuetifyHookContext) => HookResult
-    'vuetify:ready': (vuetify: ReturnType<typeof createVuetify>) => HookResult
-  }
   interface PublicRuntimeConfig {
     vuetify: VuetifyRuntimeConfig
   }
 }
 declare module 'nuxt/schema' {
-  interface NuxtHooks {
-    'vuetify:before-create': (context: VuetifyHookContext) => HookResult
-    'vuetify:configuration': (context: VuetifyHookContext) => HookResult
-    'vuetify:ready': (vuetify: ReturnType<typeof createVuetify>) => HookResult
-  }
   interface PublicRuntimeConfig {
     vuetify: VuetifyRuntimeConfig
   }
